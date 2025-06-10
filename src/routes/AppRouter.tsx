ce5adb5c1c0ab2c46a5eca041f6295b272450c3b
bagusrosfandy <bagusrosfandy@refactory.id>
2025-06-10 10:09:01 +0700

    update

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import SquadPage from "@/pages/Squad";
+import { IsLoggedIn, RequiredLogin } from "@/guard/AuthGuard";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/squad" element={<SquadPage />} />
-      <Route path="/login" element={<Login />} />
+      <Route element={<RequiredLogin />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/squad" element={<SquadPage />} />
+      </Route>
+      <Route element={<IsLoggedIn />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
